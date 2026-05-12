35d0567d1988e41ab346c3fefc0fcf0bc093c6ed
Meowouuo <9142634@qq.com>
2026-05-12 22:19:42 +0800

    Create 一生何求(live).js

---
+++ lyrics/一生何求(live).js
@@ -0,0 +1,55 @@
+// 歌曲：一生何求(live)
+
+(function() {
+    const song = {
+        id: 28,
+        title: "一生何求(live)",
+        titleJyutping: ["jat1","sang1","ho4","kau4","","","","","",""],
+        artist: "古巨基",
+        artistJyutping: ["gu2","geoi6","gei1"],
+        lyricist: "林夕",
+        lyricistJyutping: ["lam4","zik6"],
+        composer: "陈志豪",
+        composerJyutping: ["can4","zi3","hou4"],
+        lyrics: [
+            { chars: ["若","有","你","若","有","我","还","有","十","块"], jp: ["joek6","jau5","nei5","joek6","jau5","ngo5","waan4","jau5","sap6","faai3"] },
+            { chars: ["这","样","我","便","可","以","约","你","去","街"], jp: ["ze5","joeng6","ngo5","bin6","ho2","ji5","joek3","nei5","heoi3","gaai1"] },
+            { chars: ["只","有","十","块","什","么","信","物","平","便","买"], jp: ["zi2","jau5","sap6","faai3","sap6","maa1","seon3","mat6","ping4","bin6","maai5"] },
+            { chars: ["甜","蜜","蜜","吃","些","烧","卖"], jp: ["tim4","mat6","mat6","hek3","se1","siu1","maai6"] },
+            { paragraphBreak: true },
+            { chars: ["没","老","友","没","女","友","如","何","愉","快"], jp: ["mut6","lou5","jau5","mut6","neoi5","jau5","jyu4","ho4","jyu4","faai3"] },
+            { chars: ["生","命","要","用","把","尺","透","彻","了","解"], jp: ["sang1","ming6","jiu3","jung6","baa2","cek3","tau3","cit3","liu5","gaai2"] },
+            { chars: ["得","到","没","有","或","者","当","做","还","旧","债"], jp: ["dak1","dou3","mut6","jau5","waak6","ze2","dong1","zou6","waan4","gau6","zaai3"] },
+            { chars: ["事","业","完","美","得","不","解"], jp: ["si6","jip6","jyun4","mei5","dak1","bat1","gaai2"] },
+            { paragraphBreak: true },
+            { chars: ["大","概","天","要","我","两","者","挑","选"], jp: ["daai6","koi3","tin1","jiu3","ngo5","loeng5","ze2","tiu1","syun2"] },
+            { chars: ["跟","你","两","个","宁","愿","缺","少","了","人","缘"], jp: ["gan1","nei5","loeng5","go3","ning4","jyun6","kyut3","siu2","liu5","jan4","jyun4"] },
+            { chars: ["对","我","你","偏","庇","护","纵","宠"], jp: ["deoi3","ngo5","nei5","pin1","bei3","wu6","zung3","cung2"] },
+            { chars: ["事","业","仍","为","我","捐"], jp: ["si6","jip6","jing4","wai4","ngo5","gyun1"] },
+            { paragraphBreak: true },
+            { chars: ["若","要","的","通","通","也","没","有"], jp: ["joek6","jiu3","dik1","tung1","tung1","jaa5","mut6","jau5"] },
+            { chars: ["然","而","仍","可","手","挽","手"], jp: ["jin4","ji4","jing4","ho2","sau2","waan5","sau2"] },
+            { chars: ["剩","下","温","柔","问","我","一","生","复","何","求"], jp: ["sing6","haa6","wan1","jau4","man6","ngo5","jat1","sang1","fuk6","ho4","kau4"] },
+            { chars: ["庸","才","仍","可","得","到","你","问","候"], jp: ["jung4","coi4","jing4","ho2","dak1","dou3","nei5","man6","hau6"] },
+            { chars: ["无","限","滋","悠","慢","慢","细","赏","星","宿"], jp: ["mou4","haan6","zi1","jau4","maan6","maan6","sai3","soeng2","sing1","suk1"] },
+            { paragraphBreak: true },
+            { chars: ["没","有","钱","不","要","走"], jp: ["mut6","jau5","cin4","bat1","jiu3","zau2"] },
+            { chars: ["到","了","有","钱","通","处","走"], jp: ["dou3","liu5","jau5","cin4","tung1","cyu5","zau2"] },
+            { chars: ["乐","在","心","头","问","我","一","生","复","何","求"], jp: ["lok6","zoi6","sam1","tau4","man6","ngo5","jat1","sang1","fuk6","ho4","kau4"] },
+            { chars: ["庸","才","难","得","找","到","你","愿","受"], jp: ["jung4","coi4","naan4","dak1","zaau2","dou3","nei5","jyun6","sau6"] },
+            { chars: ["情","在","心","灵","在","身","外"], jp: ["cing4","zoi6","sam1","ling4","zoi6","san1","ngoi6"] },
+            { chars: ["任","一","切","没","有"], jp: ["jam6","jat1","cit3","mut6","jau5"] },
+            { paragraphBreak: true },
+            { chars: ["命","数","转","了","又","转"], jp: ["ming6","sou3","zyun2","liu5","jau6","zyun2"] },
+            { chars: ["其","实","始","终","不","自","愿"], jp: ["kei4","sat6","ci2","zung1","bat1","zi6","jyun6"] },
+            { chars: ["庸","才","能","与","你","一","起","堕","后"], jp: ["jung4","coi4","nang4","jyu5","nei5","jat1","hei2","do6","hau6"] },
+            { chars: ["落","魄","温","柔","那","温","柔"], jp: ["lok6","paak3","wan1","jau4","naa5","wan1","jau4"] },
+            { chars: ["将","火","化","万","有"], jp: ["zoeng1","fo2","faa3","maan6","jau5"] },
+            { chars: ["情","在","心","灵","你","生","存"], jp: ["cing4","zoi6","sam1","ling4","nei5","sang1","cyun4"] }
+        ]
+    };
+    window.__songs.push(song);
+}());
+
+// ⚠️ 重要：请在 index.html 的 songFiles 数组中添加以下一行：
+// 'lyrics/一生何求(live).js',
