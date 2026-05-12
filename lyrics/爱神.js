8b03e77c73c78db71890fbdd07ae386caf9466ab
Meowouuo <9142634@qq.com>
2026-05-12 21:57:58 +0800

    Create 爱神.js
    新增歌曲 爱神

---
+++ lyrics/爱神.js
@@ -0,0 +1,48 @@
+// 歌曲：爱神
+
+(function() {
+    const song = {
+        id: 16,
+        title: "爱神",
+        titleJyutping: ["ngoi3","san4"],
+        artist: "古巨基",
+        artistJyutping: ["gu2","geoi6","gei1"],
+        lyricist: "林夕",
+        lyricistJyutping: ["lam4","zik6"],
+        composer: "雷颂德",
+        composerJyutping: ["leoi4","zung6","dak1"],
+        lyrics: [
+            { chars: ["神"," ","请","你","动","凡","心"], jp: ["san4","","cing2","nei5","dung6","faan4","sam1"] },
+            { chars: ["请","给","我","回","音"," ","你","可","不","可","转","身"], jp: ["cing2","kap1","ngo5","wui4","jam1","","nei5","ho2","bat1","ho2","zyun2","san1"] },
+            { chars: ["知","你","爱","世","人"," ","才","胆","敢","诚","邀","你","让","我","等"], jp: ["zi1","nei5","ngoi3","sai3","jan4","","coi4","daam2","gam2","sing4","jiu1","nei5","joeng6","ngo5","dang2"] },
+            { chars: ["人","类","太","不","合","衬","妄","想","合","衬"," ","才","因","此","伤","得","更","深"], jp: ["jan4","leoi6","taai3","bat1","hap6","can3","mong5","soeng2","hap6","can3","","coi4","jan1","ci2","soeng1","dak1","gang1","sam1"] },
+            { paragraphBreak: true },
+            { chars: ["神"," ","你","可","否","恋","上","我"," ","神","圣","不","可","侵","我","么"], jp: ["san4","","nei5","ho2","fau2","lyun2","soeng6","ngo5","","san4","sing3","bat1","ho2","cam1","ngo5","maa1"] },
+            { chars: ["情","关","得","不","到","你","相","助"," ","谁","又","肯","被","我","触","摸"], jp: ["cing4","gwaan1","dak1","bat1","dou3","nei5","soeng1","zo6","","seoi4","jau6","hang2","bei6","ngo5","zuk1","mo2"] },
+            { chars: ["僧","人","都","不","喜","爱","我"," ","神","你","不","欢","喜","我"," ","迫","我","入","了","魔"], jp: ["zang1","jan4","dou1","bat1","hei2","ngoi3","ngo5","","san4","nei5","bat1","fun1","hei2","ngo5","","baak1","ngo5","jap6","liu5","mo1"] },
+            { chars: ["爱","侣","几","百","万"," ","谁","料","我","蠢","得","竟","可","重","复","去","犯","错"], jp: ["ngoi3","leoi5","gei2","baak3","maan6","","seoi4","liu6","ngo5","ceon2","dak1","ging2","ho2","cung5","fuk6","heoi3","faan6","co3"] },
+            { paragraphBreak: true },
+            { chars: ["人"," ","恋","爱","极","残","忍"], jp: ["jan4","","lyun2","ngoi3","gik6","caan4","jan2"] },
+            { chars: ["分","手","更","难","堪"," ","连","家","私","都","要","分"], jp: ["fan1","sau2","gang1","naan4","ham1","","lin4","gaa1","si1","dou1","jiu3","fan1"] },
+            { chars: ["等","我","那","女","神","来","亲","吻"," ","神","迹","哪","日","发","生"], jp: ["dang2","ngo5","naa5","neoi5","san4","loi4","can1","man5","","san4","zik1","naa5","jat6","faat3","sang1"] },
+            { chars: ["无","论","有","多","合","衬","纵","不","合","衬","人","只","想","追","超","快","感"], jp: ["mou4","leon6","jau5","do1","hap6","can3","zung3","bat1","hap6","can3","jan4","zi2","soeng2","zeoi1","ciu1","faai3","gam2"] },
+            { paragraphBreak: true },
+            { chars: ["神"," ","你","可","否","恋","上","我"," ","神","圣","不","可","侵","我","么"], jp: ["san4","","nei5","ho2","fau2","lyun2","soeng6","ngo5","","san4","sing3","bat1","ho2","cam1","ngo5","maa1"] },
+            { chars: ["情","关","得","不","到","你","相","助"," ","谁","又","肯","被","我","触","摸"], jp: ["cing4","gwaan1","dak1","bat1","dou3","nei5","soeng1","zo6","","seoi4","jau6","hang2","bei6","ngo5","zuk1","mo2"] },
+            { chars: ["僧","人","都","不","喜","爱","我"," ","神","你","不","欢","喜","我"," ","迫","我","入","了","魔"], jp: ["zang1","jan4","dou1","bat1","hei2","ngoi3","ngo5","","san4","nei5","bat1","fun1","hei2","ngo5","","baak1","ngo5","jap6","liu5","mo1"] },
+            { chars: ["爱","侣","几","百","万"," ","谁","料","我","永","远","只","可","回","忆","我","上","个"], jp: ["ngoi3","leoi5","gei2","baak3","maan6","","seoi4","liu6","ngo5","wing5","jyun5","zi2","ho2","wui4","jik1","ngo5","soeng6","go3"] },
+            { paragraphBreak: true },
+            { chars: ["缠","绕","到","下","个","重","复","恶","梦"," ","毫","无","力","气","伸","手","去","拖"], jp: ["cin4","jiu2","dou3","haa6","go3","cung5","fuk6","ok3","mung6","","hou4","mou4","lik6","hei3","san1","sau2","heoi3","to1"] },
+            { chars: ["怎","么","可","帮","我","战","胜","心","魔"," ","难","道","你","看","不","清","楚"], jp: ["zam2","maa1","ho2","bong1","ngo5","zin3","sing3","sam1","mo1","","naan4","dou6","nei5","hon3","bat1","cing1","co2"] },
+            { paragraphBreak: true },
+            { chars: ["神"," ","你","可","否","恋","上","我"," ","神","圣","不","可","侵","我","么"], jp: ["san4","","nei5","ho2","fau2","lyun2","soeng6","ngo5","","san4","sing3","bat1","ho2","cam1","ngo5","maa1"] },
+            { chars: ["情","关","得","不","到","你","相","助"," ","谁","又","肯","被","我","触","摸"], jp: ["cing4","gwaan1","dak1","bat1","dou3","nei5","soeng1","zo6","","seoi4","jau6","hang2","bei6","ngo5","zuk1","mo2"] },
+            { chars: ["僧","人","都","不","喜","爱","我"," ","神","你","不","欢","喜","我"," ","迫","我","入","了","魔"], jp: ["zang1","jan4","dou1","bat1","hei2","ngoi3","ngo5","","san4","nei5","bat1","fun1","hei2","ngo5","","baak1","ngo5","jap6","liu5","mo1"] },
+            { chars: ["爱","侣","几","百","万"," ","谁","料","我","蠢","得","竟","可","重","复","去","犯","错"], jp: ["ngoi3","leoi5","gei2","baak3","maan6","","seoi4","liu6","ngo5","ceon2","dak1","ging2","ho2","cung5","fuk6","heoi3","faan6","co3"] }
+        ]
+    };
+    window.__songs.push(song);
+}());
+
+// ⚠️ 重要：请在 index.html 的 songFiles 数组中添加以下一行：
+// 'lyrics/爱神.js',
